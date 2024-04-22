4d1fb7864b1786400ac9c613a0a5ec0c95a7c186
github-action-benchmark <github@users.noreply.github.com>
2024-04-22 16:31:07 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 0e563584605a2e2b8419c39a1323badb4af8a508

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1713801728748,
+  "lastUpdate": 1713803467912,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -33,6 +33,65 @@ window.BENCHMARK_DATA = {
           {
             "name": "neureka_basic_tests:t6",
             "value": 494,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "FrancescoConti@users.noreply.github.com",
+            "name": "Francesco Conti",
+            "username": "FrancescoConti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0e563584605a2e2b8419c39a1323badb4af8a508",
+          "message": "Merge branch 'hci-v2' into add-perf-regr-rebase",
+          "timestamp": "2024-04-22T18:19:15+02:00",
+          "tree_id": "ae51ff2d1149159cf51600d2370de684dfc264a7",
+          "url": "https://github.com/pulp-platform/neureka/commit/0e563584605a2e2b8419c39a1323badb4af8a508"
+        },
+        "date": 1713803467507,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 572,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t3",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 494,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
             "unit": "cycles"
           }
         ]
