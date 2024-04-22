c813229073187ad036e4f30a0d90e026a1f78849
github-action-benchmark <github@users.noreply.github.com>
2024-04-22 16:02:08 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 9788b2f0b1afe6773d6ffda735df28e9857f8bc6

---
+++ dev/bench/data.js
@@ -0,0 +1,42 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1713801728748,
+  "repoUrl": "https://github.com/pulp-platform/neureka",
+  "entries": {
+    "Execution cycles": [
+      {
+        "commit": {
+          "author": {
+            "email": "f.conti@unibo.it",
+            "name": "Francesco Conti",
+            "username": "FrancescoConti"
+          },
+          "committer": {
+            "email": "f.conti@unibo.it",
+            "name": "Francesco Conti",
+            "username": "FrancescoConti"
+          },
+          "distinct": true,
+          "id": "9788b2f0b1afe6773d6ffda735df28e9857f8bc6",
+          "message": "Merge branch 'ci2' into add-perf-regr",
+          "timestamp": "2024-04-22T17:45:32+02:00",
+          "tree_id": "3a1b62eb86cc94270cdbe30184bdc3537c9aeed7",
+          "url": "https://github.com/pulp-platform/neureka/commit/9788b2f0b1afe6773d6ffda735df28e9857f8bc6"
+        },
+        "date": 1713801727883,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 494,
+            "unit": "cycles"
+          }
+        ]
+      }
+    ]
+  }
+}
