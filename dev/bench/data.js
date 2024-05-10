3de013864c9eded235fe3b56137fcce2706b11bf
github-action-benchmark <github@users.noreply.github.com>
2024-05-10 08:28:37 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 0337dcf856a7a2bfa72df39d234831646f1015f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715329716445,
+  "lastUpdate": 1715329717625,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -513,6 +513,61 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/pulp-platform/neureka/commit/0337dcf856a7a2bfa72df39d234831646f1015f4"
         },
         "date": 1715329715888,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t3",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 572,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 494,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 142,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "committer": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "id": "0337dcf856a7a2bfa72df39d234831646f1015f4",
+          "message": "Astral configuration",
+          "timestamp": "2024-05-01T22:00:09Z",
+          "url": "https://github.com/pulp-platform/neureka/pull/1/commits/0337dcf856a7a2bfa72df39d234831646f1015f4"
+        },
+        "date": 1715329717061,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
