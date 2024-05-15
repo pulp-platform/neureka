7a8a523fe3dfedccb073190d545a54daf8d1ae29
github-action-benchmark <github@users.noreply.github.com>
2024-05-15 16:36:47 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 2dfe9ef208681eea9abec68d81f848b9454c4b8c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1715759500634,
+  "lastUpdate": 1715791007934,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -1345,6 +1345,65 @@ window.BENCHMARK_DATA = {
           },
           {
             "name": "neureka_basic_tests:t1",
+            "value": 142,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "FrancescoConti@users.noreply.github.com",
+            "name": "Francesco Conti",
+            "username": "FrancescoConti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2dfe9ef208681eea9abec68d81f848b9454c4b8c",
+          "message": "Update standalone TB ibex dependency",
+          "timestamp": "2024-05-15T18:27:01+02:00",
+          "tree_id": "d73fe02d36479c611582c32c92eda76a7e1af1d1",
+          "url": "https://github.com/pulp-platform/neureka/commit/2dfe9ef208681eea9abec68d81f848b9454c4b8c"
+        },
+        "date": 1715791007545,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 572,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 494,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t3",
             "value": 142,
             "unit": "cycles"
           }
