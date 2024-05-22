850911f938f1d2a7c91a61f991e8768b3d573b7f
github-action-benchmark <github@users.noreply.github.com>
2024-05-22 21:37:41 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 834c998cc72c53a4084f5db49fde07b5d248454a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716413419840,
+  "lastUpdate": 1716413861005,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -1854,6 +1854,61 @@ window.BENCHMARK_DATA = {
           {
             "name": "neureka_basic_tests:t2",
             "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 494,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "committer": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "id": "834c998cc72c53a4084f5db49fde07b5d248454a",
+          "message": "Astral configuration",
+          "timestamp": "2024-05-16T11:58:42Z",
+          "url": "https://github.com/pulp-platform/neureka/pull/1/commits/834c998cc72c53a4084f5db49fde07b5d248454a"
+        },
+        "date": 1716413860609,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 572,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t3",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 142,
             "unit": "cycles"
           },
           {
