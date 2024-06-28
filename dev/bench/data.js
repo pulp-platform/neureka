4e05ca9934eb525fbb770020885d4ca43b46f071
github-action-benchmark <github@users.noreply.github.com>
2024-06-28 16:13:42 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for 7f991e5b792872c8d7892a49050b8c58804f94f4

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719329354827,
+  "lastUpdate": 1719591222557,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -2956,6 +2956,61 @@ window.BENCHMARK_DATA = {
           {
             "name": "neureka_basic_tests:t3",
             "value": 143,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "committer": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "id": "7f991e5b792872c8d7892a49050b8c58804f94f4",
+          "message": "Integrate ECC-extended HCI",
+          "timestamp": "2024-06-02T12:41:59Z",
+          "url": "https://github.com/pulp-platform/neureka/pull/9/commits/7f991e5b792872c8d7892a49050b8c58804f94f4"
+        },
+        "date": 1719591222105,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t3",
+            "value": 135,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t6",
+            "value": 497,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 135,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 135,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 562,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 135,
             "unit": "cycles"
           }
         ]
