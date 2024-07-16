6e5e83d8a1478c850bcc4cd1160b913dcc8cf5ac
github-action-benchmark <github@users.noreply.github.com>
2024-07-16 13:15:07 +0000

    add Execution cycles (customSmallerIsBetter) benchmark result for b3a6dab2a0c034e339c5bdf4e334f260255361e5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1721135704078,
+  "lastUpdate": 1721135707098,
   "repoUrl": "https://github.com/pulp-platform/neureka",
   "entries": {
     "Execution cycles": [
@@ -3370,6 +3370,56 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/pulp-platform/neureka/commit/b3a6dab2a0c034e339c5bdf4e334f260255361e5"
         },
         "date": 1721135703586,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "neureka_basic_tests:t1",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t0",
+            "value": 140,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t3",
+            "value": 142,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t4",
+            "value": 572,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t5",
+            "value": 404,
+            "unit": "cycles"
+          },
+          {
+            "name": "neureka_basic_tests:t2",
+            "value": 140,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "committer": {
+            "name": "pulp-platform",
+            "username": "pulp-platform"
+          },
+          "id": "b3a6dab2a0c034e339c5bdf4e334f260255361e5",
+          "message": "[fix] new HCI to enable bandwidth required for 3x3 mode",
+          "timestamp": "2024-07-15T23:48:57Z",
+          "url": "https://github.com/pulp-platform/neureka/pull/15/commits/b3a6dab2a0c034e339c5bdf4e334f260255361e5"
+        },
+        "date": 1721135706775,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
