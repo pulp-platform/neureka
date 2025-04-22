1b3bd6dcdafcf7bba8c9153ff6397b0671a88d62
Luigi Ghionda <luigi.ghionda2@unibo.it>
2025-04-22 17:25:22 +0200

    Add regression script to test different PEs configurations
    Only configurations covered by this script have been tested; others are unverified.

---
+++ regr/conf_regression.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Copyright (C) 2020-2024 ETH Zurich and University of Bologna
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Author: Luigi Ghionda (luigi.ghionda2@unibo.it)
+
+export N_PROC=20
+export P_STALL=0.04
+TIMEOUT=200
+
+# (H, W)
+PARAMS=(
+    2 2
+    3 3
+    4 4
+    5 5
+    4 8
+    6 6
+    4 2
+    2 4
+    3 4
+    4 3
+)
+
+# Declare a string array with type
+declare -a test_list=(
+    "regr/basic.yml"
+#    "regr/fs1.yml"
+)
+
+i=0
+while [[ $i -lt ${#PARAMS[@]} ]]; do
+    export PE_H=${PARAMS[$i]}
+    export PE_W=${PARAMS[$((i + 1))]}
+    i=$((i + 2))
+
+    echo "Running with config (H, W)=($PE_H, $PE_W)"
+
+    # Read the list values with space
+    for val in "${test_list[@]}"; do
+        nice -n10 regr/bwruntests.py --report_junit -t ${TIMEOUT} --yaml -o regr/neureka_tests.xml -p${N_PROC} --perf regr/perf.json $val
+        if test $? -ne 0; then
+            echo "Error in test $val with config (H, W)=($PE_H, $PE_W)"
+            exit 1
+        fi
+    done
+done
+unset P_STALL
+unset PE_H
+unset PE_W
