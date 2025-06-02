01ff0f5742cc7dbc4af1e65ca046bd2d0ed88c40
aottaviano <aottaviano@iis.ee.ethz.ch>
2025-06-02 14:23:10 +0200

    rtl: Move wildcard imports to module header
    Global wildcard imports may create name collision, as indicated in:
    https://github.com/lowRISC/style-guides/blob/master/VerilogCodingStyle.md#wildcard-import-of-packages

--- rtl/accumulator/neureka_normquant_multiplier.sv
+++ rtl/accumulator/neureka_normquant_multiplier.sv
@@ -20,9 +20,8 @@
  * Authors (NEUREKA): Arpan Suravi Prasad <prasadar@iis.ee.ethz.ch>
  */
 
-import neureka_package::*;
 
-module neureka_normquant_multiplier #(
+module neureka_normquant_multiplier import neureka_package::*; #(
   parameter int unsigned NMS = neureka_package::NORM_MULT_SIZE,
   parameter int unsigned ACC = neureka_package::NEUREKA_ACCUM_SIZE,
   parameter int unsigned PIPE = 0
